788bd4a08eddcd9f34c56a9f1a0e53e9e86a7c82
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-09-18 10:37:16 +0200

    objectFit:none

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -69,7 +69,7 @@ export const MovieCard = ({ movie }) => {
             <div className="card-content">  
                 <Card.Body>    
                     <div className="clickable-area" onClick={handleCardClick} style={{ cursor: 'pointer', flexGrow: 1 }}>
-                        <Card.Img variant="top" src={movie.image} />
+                        <Card.Img variant="top" src={movie.image} style={{ objectFit: 'none' }} />
                         <Card.Title>{movie.title}</Card.Title>
                         <div className="card-text-container">
                             <Card.Text>{movie.actors.join(" & ")}</Card.Text>
